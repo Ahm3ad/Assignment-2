d03a563709918ea97858a69ec40d086bdbaedf4e
Ahmaad Riaz <ahmaad.riaz@ontariotechu.net>
2024-11-06 22:16:24 -0500

    updating server
    ahhah
    crying

--- Assignment2/routes/index.js
+++ Assignment2/routes/index.js
@@ -1,33 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Render the home page */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Home'
   });
 });
 
-/* GET home page (duplicate route, can be removed or redirected). */
+/* Redirect '/home' to the root URL to avoid duplication */
 router.get('/home', function(req, res, next) {
   res.redirect('/');
 });
 
-/* GET about page. */
+/* Render the about page */
 router.get('/about', function(req, res, next) {
   res.render('about', { 
     title: 'About Me'
   });
 });
 
-/* GET projects page. */
+/* Render the projects page */
 router.get('/projects', function(req, res, next) {
   res.render('projects', { 
     title: 'Projects'
   });
 });
 
-/* GET contact page. */
+/* Render the contact page */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { 
     title: 'Contact Me'
